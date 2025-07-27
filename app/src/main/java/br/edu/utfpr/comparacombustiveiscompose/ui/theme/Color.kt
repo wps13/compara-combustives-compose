891d337e9462cbc05475b2caaa44c0f544fe5110
Willane Paiva <willanepaiva@hotmail.com>
2025-07-27 09:15:30 -0300

    configure theme

--- app/src/main/java/br/edu/utfpr/comparacombustiveiscompose/ui/theme/Color.kt
+++ app/src/main/java/br/edu/utfpr/comparacombustiveiscompose/ui/theme/Color.kt
@@ -1,11 +1,13 @@
 package br.edu.utfpr.comparacombustiveiscompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightPrimary = Color(0xFFcc4137)
+
+val LightSecondary = Color(0xFFc07f78)
+
+val DarkPrimary = Color(0xFFdb7a73)
+
+val DarkSecondary = Color(0xFFcba59f)
